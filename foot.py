5d384257fc5efce0b24f9c0e6510c22f2c4522d7
cstepien <courtney.stepien@gmail.com>
2018-11-24 19:20:53 -0600

    Launch app and redraw foot when window is resized

---
+++ foot.py
@@ -0,0 +1,40 @@
+from kivy.uix.widget import Widget
+from kivy.graphics import *
+from kivy.core.window import Window
+
+class Foot(Widget):
+
+	verteces = [-15, 25, 
+		15, 25, 
+		15,-25, 
+		3, -13, 
+		3, -5, 
+		10, -5, 
+		10, 5, 
+		-10, 5, 
+		-10, -5, 
+		-3, -5, 
+		-3, -13, 
+		-15, -25]
+
+	def __init__(self, **kwargs):
+		super(Foot, self).__init__(**kwargs)
+		self.size = Window.size
+
+		with self.canvas:
+			Color(1.0, 1.0, 1.0)
+			self.lines = Line(points= self.recentered_verteces((self.center_x, self.center_y)), 
+						close=True)
+
+	def center_foot(self, new_center):
+		self.lines.points = self.recentered_verteces(new_center)
+
+	def recentered_verteces(self, coord):
+		new_points = []
+		for index, num in enumerate(self.verteces):
+			if index%2 == 0:
+				new_points.append(self.verteces[index]+coord[0])
+			elif index%2 == 1:
+				new_points.append(self.verteces[index]+coord[1])
+
+		return new_points
---
+++ main.py
@@ -0,0 +1,28 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.anchorlayout import AnchorLayout
+import foot
+from kivy.core.window import Window
+from kivy.graphics import Line
+
+class SewingGame(Widget):
+	def __init__(self, **kwargs):
+		super(SewingGame, self).__init__(**kwargs)
+		self.size = Window.size
+
+		self.bind(size=self.adapt)
+		#self.on_size = self.adapt
+
+		self.foot = foot.Foot()
+		self.add_widget(self.foot)
+		
+	def adapt(self, Inst, size):
+		self.foot.center_foot((self.center_x, self.center_y))
+
+class SewingApp(App):
+	def build(self):
+		return SewingGame()
+
+
+if __name__ == '__main__':
+	SewingApp().run()
