a573ea9389dc0ee08136c5cb368c8254b0b2a7cb
LAPTOP-VUSINORT\bobby <bobbylox@gmail.com>
2018-11-25 19:25:25 -0800

    Working on adding stitches (not there yet)

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ import foot
 import piece
 from kivy.core.window import Window
 from kivy.graphics import Line
+from kivy.clock import Clock
 
 class SewingGame(Widget):
 	def __init__(self, **kwargs):
 		super(SewingGame, self).__init__(**kwargs)
 		self.size = Window.size
 
-		self.add_widget(piece.Piece("assets/CCDBlueTee.png"))
+		self.piece = piece.Piece("assets/CCDBlueTee.png")
+		self.add_widget(self.piece)
+		Clock.schedule_interval(self.needle_down, 0.5)
 
 		self.bind(size=self.adapt)
 		#self.on_size = self.adapt
@@ -19,9 +22,16 @@ class SewingGame(Widget):
 		self.foot = foot.Foot()
 		self.add_widget(self.foot)
 		self.foot.center_foot((self.center_x, self.center_y))
-		
+	
 	def adapt(self, Inst, size):
 		self.foot.center_foot((self.center_x, self.center_y))
+
+	def needle_down(self, dt):
+		#figure out where the center is
+		foot_on_piece = (self.center_x - self.piece.pos[0], self.center_y - self.piece.pos[1])
+		#pass the center to self.piece.add_stitch_coord()
+		self.piece.add_stitch_coord(foot_on_piece)
+
 
 class SewingApp(App):
 	def build(self):
--- piece.py
+++ piece.py
@@ -1,6 +1,8 @@
 from kivy.uix.scatter import Scatter
 from kivy.uix.image import Image
 from kivy.properties import StringProperty
+from kivy.graphics import *
+from kivy.graphics.instructions import InstructionGroup
 
 class Piece(Scatter):
 
@@ -12,3 +14,28 @@ class Piece(Scatter):
 		self.scale = self.image.texture_size[0]/self.size[0]
 		self.do_scale = False	
 		self.add_widget(self.image)
+		
+		self.instructions = InstructionGroup()
+		self.canvas.add(self.instructions)
+		self.stitch_coords = [(0,0)]
+		self.end_stitch((self.center_x, self.center_y))
+
+		'''with self.canvas:
+			Color(1,1,1)
+			Line(points = [self.center_x, self.center_y, 0, 0])
+		'''
+
+	def start_stitch(self, coord):
+		self.stitch_coords.append(coord)
+
+	def end_stitch(self, coord):
+		self.stitch_coords.append(coord)
+		self.instructions.add(Line(points=[self.stitch_coords[-2][0], self.stitch_coords[-2][1], self.stitch_coords[-1][0],self.stitch_coords[-1][1]]))
+
+	def add_stitch_coord(self, coord):
+		if len(self.stitch_coords)%2==0:
+			self.start_stitch(coord)
+		else:
+			self.end_stitch(coord)
+
+		print(self.stitch_coords)
