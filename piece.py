e341b7d3cb008a85c0bfe730f668f1a3b4b76e8f
cstepien <courtney.stepien@gmail.com>
2018-11-26 19:43:09 -0600

    Rescaled stitch coordinates, foot is 'sewing', commented next steps

--- piece.py
+++ piece.py
@@ -11,14 +11,19 @@ class Piece(Scatter):
 		self.auto_bring_to_front = False
 		self.source = source # consider using StringProperty later so we can update redraw with new sources
 		self.image = Image(source=self.source)
+		# why did we do scale again? 
 		self.scale = self.image.texture_size[0]/self.size[0]
 		self.do_scale = False	
 		self.add_widget(self.image)
 		
 		self.instructions = InstructionGroup()
 		self.canvas.add(self.instructions)
+		# This starting point is incorrect - leads to drawing a line from (0,0) to first position for first line
+		# changing initial coords to self.center_x, self.center_y has the same result
+		# I think we need some special case in add_stitch_coord for the first stitch
 		self.stitch_coords = [(0,0)]
-		self.end_stitch((self.center_x, self.center_y))
+		print("Self size:", self.size, "texture size:", self.image.texture_size, "Scale:", self.scale)
+		#self.end_stitch((self.center_x, self.center_y))
 
 		'''with self.canvas:
 			Color(1,1,1)
@@ -28,9 +33,15 @@ class Piece(Scatter):
 	def start_stitch(self, coord):
 		self.stitch_coords.append(coord)
 
+		# end_stitch currently can't deal with rotating the piece - it breaks the drawing function
+		# Probably due to coordinates being messed up relative to the screen
+		# there are options in Scatter to translate coordinates to scattered position
 	def end_stitch(self, coord):
 		self.stitch_coords.append(coord)
-		self.instructions.add(Line(points=[self.stitch_coords[-2][0], self.stitch_coords[-2][1], self.stitch_coords[-1][0],self.stitch_coords[-1][1]]))
+		self.instructions.add(Line(points=[self.stitch_coords[-2][0]/self.scale, 
+			self.stitch_coords[-2][1]/self.scale, 
+			self.stitch_coords[-1][0]/self.scale,
+			self.stitch_coords[-1][1]/self.scale]))
 
 	def add_stitch_coord(self, coord):
 		if len(self.stitch_coords)%2==0:
